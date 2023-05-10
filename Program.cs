14851d4971731292f03fb5f05e8113b950ede877
Rafael <rafaelratier@hotmail.com>
2023-05-09 22:57:40 -0300

    desafio completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia cel1 = new Nokia("1397978787", "Nokia Z70", "123456", 4);
+
+cel1.InstalarAplicativo("YouTube");
+
+Iphone cel2 = new Iphone("1386688668", "Iphone XR", "54321", 8);
+
+cel2.InstalarAplicativo("Stellarium");
